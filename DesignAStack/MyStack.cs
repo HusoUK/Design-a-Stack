bcd7166d4afa2da5cd7a50dc7b92f00a32676195
Richard Wilkinson <richwilk.iphone@gmail.com>
2022-11-16 17:00:44 +0000

    Initial Code Commit
    Added the code for the console app

---
+++ DesignAStack/MyStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignAStack
+{
+    public class MyStack
+    {
+        private readonly List<object> _stack = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("You can not add a null");
+            else
+                _stack.Add(obj);
+        }
+
+        public string Pop()
+        {
+            if (_stack.Count > 0)
+            {
+                int removeValue = _stack.Count - 1;
+                string result = _stack[removeValue].ToString();
+                _stack.RemoveAt(removeValue);
+                return result;
+            }
+            else
+                throw new InvalidOperationException("Can not clear nothing");
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+
+        public void Display()
+        {
+            foreach (var obj in _stack)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+        }
+    }
+}
---
+++ DesignAStack/Program.cs
@@ -0,0 +1,23 @@
+﻿using DesignAStack;
+using System.Collections;
+
+var stack = new MyStack();
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+stack.Push('A');
+stack.Push("BBbb");
+stack.Push(null);
+
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+
+Console.WriteLine("----------------------------------------\n----------------------------------------");
+
+stack.Display();
+
+Console.WriteLine("----------------------------------------\n----------------------------------------");
+stack.Clear();
+stack.Display();
+
